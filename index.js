6fe434b199a4e1eed89278dc0bbc544d6bb2d41a
CDESLAYR <cdeslayr@gmail.com>
2019-05-10 06:25:29 +0200

    Reorganize directory

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // setup config first before anything by requiring it
 var config = require('./server/config/config');
-var app = require('./server/server');
+var app = require('./server/app');
 
 app.listen(config.port);
 console.log('listening on http://localhost:' + config.port);
